85594b2b17d0d83630036762e02f8d2c3de8d5bc
zjzhang <zhangzj33@gmail.com>
2024-06-27 15:45:03 +0800

    first commit

---
+++ example/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	annoyindex "github.com/oligo/annoy-go"
+)
+
+const indexFile = "./embeddings.ann"
+
+func main() {
+	embeddings := [][]float32{
+		{0.1, 0.2, 0.31},
+		{0.4, 0.55, 0.67},
+	}
+
+	index := annoyindex.NewAnnoyIndexAngular(len(embeddings[0]))
+	if PathExists(indexFile) {
+		if ret := index.Load(indexFile, false); !ret {
+			panic("Failed to load file without prefault")
+		}
+	} else {
+		defer func() {
+			index.Build(5)
+			index.Save(indexFile, false)
+		}()
+		for i, embedding := range embeddings {
+			index.AddItem(i, embedding)
+		}
+	}
+
+	distance1 := index.GetDistance(0, 1)
+	distance2 := index.GetDistance(0, 2)
+	fmt.Printf("distance between 1 and 2: %f, 0 and 3: %f\n", distance1, distance2)
+
+	// query with a vector
+	query := []float32{0.12, 0.22, 0.3}
+	result := []int{}
+	distances := []float32{}
+	index.GetNnsByVector(query, 10, -1, &result, &distances)
+
+	for i, idx := range result {
+		vector := []float32{}
+		index.GetItem(idx, &vector)
+		fmt.Printf("found vector: %v, distance: %f\n", result, distances[i])
+	}
+}
+
+func PathExists(path string) bool {
+	stat, err := os.Stat(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+
+	if err == nil || stat != nil {
+		return true
+	}
+
+	return false
+}
